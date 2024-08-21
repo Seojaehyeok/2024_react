99fb445678e5cd86a8b6051c076efdaf7d0623f9
Seojaehyeok <105799856+Seojaehyeok@users.noreply.github.com>
2024-08-21 19:49:08 +0900

    Update Feed.jsx

--- src-수정(필)/component/FeedComponent/Feed.jsx
+++ src-수정(필)/component/FeedComponent/Feed.jsx
@@ -2,26 +2,13 @@ import Feedbottom2 from "./bottom/Feedbottom2";
 import FeedHeader from "./header/FeedHeader";
 import FeedSnap from "./photo/FeedSnap";
 
-const Feed = () => {
-
-    const data = {
-        image:"/story/story0.jpg",
-        name: "Joshua",
-        isPopular: false,
-        city: "Tokyo",
-        nation: "Japan",
-        images: ["sapporo.jpg", "osaka.jpg", "tokyo.jpg"],
-        likePeople : ["craig_live", "jeanseal", "seo", "bae", "woo", "choi", "kim"],
-        date: "September 19",
-    };
-
-    
+const Feed = (props) => {
 
     return(
     <>
-        <FeedHeader {...data}/>
-        <FeedSnap {...data}/>
-        <Feedbottom2/>
+        <FeedHeader {...props}/>
+        <FeedSnap {...props}/>
+        <Feedbottom2 {...props}/>
             
     </>
     )
